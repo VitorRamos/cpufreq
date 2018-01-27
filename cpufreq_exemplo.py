622e158946bd05b49b88a7eb9e20279f4e44244a
Vitor Ramos <ramos.vitor89@gmail.com>
2018-01-27 10:46:28 -0300

    change example

--- cpufreq_exemplo.py
+++ cpufreq_exemplo.py
@@ -1,23 +1,25 @@
 import cpufreq
 import time
 
-x= cpufreq.CPUFreq()
-x.lits_current_governos()
-x.list_current_frequencies()
+cpu= cpufreq.CPUFreq()
+cpu.lits_current_governos()
+cpu.list_current_frequencies()
 
-x.change_governo("userspace")
-freq= x.get_frequencies()[0]['data']
+cpu.change_governo("userspace")
+cpu.get_frequencies() # list of possible frequencies for all cpus
+freq= cpu.get_frequencies()[0]['data'] #cpu frequencies
+#freq= cpu.get_frequencies()[0]['cpu'] #cpu number
 for f in freq:
-	print "Frequencia ", f
-	x.change_frequency(f)
-	x.list_current_frequencies()
+	print "Frequency ", f
+	cpu.change_frequency(f) # change frequency all cpus
+	cpu.list_current_frequencies()
 	time.sleep(1)
 
-x.change_governo("ondemand")
-x.lits_current_governos()
-x.list_current_frequencies()
+cpu.change_governo("ondemand")
+cpu.lits_current_governos()
+cpu.list_current_frequencies()
 
-x.disable_cpu(3)
+cpu.disable_cpu(3)
 time.sleep(5)
-x.enable_cpu(3)
+cpu.enable_cpu(3)
 time.sleep(5)
