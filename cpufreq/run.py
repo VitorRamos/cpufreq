cd658d24b38f0322846223be2d4d01594250b292
Alex Furtunato <alex.furtunato@ifrn.edu.br>
2019-05-29 10:06:49 -0300

    args test adjusts

--- cpufreq/run.py
+++ cpufreq/run.py
@@ -5,22 +5,88 @@
 
 """
 
+import argparse
 from cpufreq import cpuFreq,CPUFreqErrorInit
 
-def test1():
-    c = cpuFreq()
-    g = c.get_governors()
-    print(g)
 
-def test2():
-    c = cpuFreq()
-    f = c.get_max_freq()
-    print(f)
+def argsparselist(txt):
+    """
+    Validate a list of txt argument.
 
-def main():
+    :param txt: argument with comma separated int strings.
+    :return: list of strings.
     """
-    Main function executed from console run.
+
+    txt = txt.split(',')
+    listarg = [i.strip() for i in txt]
+    return listarg
+
+def argsparseintlist(txt):
     """
+    Validate a list of int arguments.
+
+    :param txt: argument with comma separated numbers.
+    :return: list of integer converted numbers.
+    """
+
+    txt = txt.split(',')
+    listarg = [int(i) for i in txt]
+    return listarg
+
+def argsparsevalidation():
+    """
+    Validation of script arguments passed via console.
+
+    :return: argparse object with validated arguments.
+    """
+
+    parser = argparse.ArgumentParser(description='Script to get and set '
+                                                 'frequencies configurations'
+                                                 'of cpus by command line')
+    subparsers = parser.add_subparsers(help="Available commands")
+
+    parse_info = subparsers.add_parser('info', 
+                                       help='print status of governors and frequencies', 
+                                       default=1)
+
+    parse_reset = subparsers.add_parser('reset', 
+                                        help='reset the governors and max and min frequencies', 
+                                        default=1)
+
+    # parser.add_argument('c', type=argsparseintlist,
+    #                     help='List of cores numbers to be '
+    #                          'used. Ex: 1,2,4')
+    # parser.add_argument('-p', '--package', help='Package Name to run',
+    #                     required=True)
+    # parser.add_argument('-c', '--compiler',
+    #                     help='Compiler name to be used on run. '
+    #                          '(Default: %(default)s).',
+    #                     choices=compilerchoicebuilds, default='gcc-hooks')
+    # parser.add_argument('-f', '--frequency', type=argsparseintlist,
+    #                     help='List of frequencies (KHz). Ex: 2000000,2100000')
+    # parser.add_argument('-i', '--input', type=argsparseinputlist,
+    #                     help=helpinputtxt, default='native')
+    # parser.add_argument('-r', '--repetitions', type=int,
+    #                     help='Number of repetitions for a specific run. '
+    #                          '(Default: %(default)s)', default=1)
+    # parser.add_argument('-b', '--cpubase', type=int,
+    #                     help='If run with thread affinity(limiting the '
+    #                          'running cores to defined number of cores), '
+    #                          'define the cpu base number.')
+    parser.add_argument('info', 
+                        help='print status of governors and frequencies', default=1)
+    args = parser.parse_args()
+    return args
+
+def reset():
+    try:
+        c = cpuFreq()
+        c.reset()
+        print("Governors, maximum and minimum frequencies reset successfully.")
+    except CPUFreqErrorInit as err:
+        print("{0}".format(err))    
+    
+def info():
     try:
         c = cpuFreq()
         print("Informations about the System:")
@@ -36,7 +102,20 @@ def main():
         for c in govs["cpu"].keys():
             print("{:4d} - {:>12} - {:>8} - {:>8} - {:>8}".format(c,govs["cpu"][c],freqs["cpu"][c],minfreq["cpu"][c],maxfreq["cpu"][c]))
     except CPUFreqErrorInit as err:
-        print("{0}".format(err))
+        print("{0}".format(err))    
+
+def main():
+    """
+    Main function executed from console run.
+    """
+    args = argsparsevalidation()
+    
+    print(args)
+    if args.info:
+        info()
+    elif args.reset:
+        reset()
+
 
 
 if __name__ == '__main__':
